5261ffb904f06c4d4c3cb9b451d42cb23638df45
lopezlin <morgan_pirata2003@yahoo.com.ar>
2020-10-13 21:00:36 -0300

    Abstract class Material created, and Durability and test01 created

---
+++ Test/Attributes/DurabilityTest.java
@@ -0,0 +1,29 @@
+package Attributes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DurabilityTest {
+
+//-------------------------------------------------------------------
+
+    @Test
+    public void test01DurebilityInstanceCreated () {
+
+        Durability durability = new Durability(10);
+        Assert.assertNotNull(durability);
+    }
+
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+
+}
