0e78c334a56d2c129e3a607d2a4876c1efaddaa1
lopezlin <morgan_pirata2003@yahoo.com.ar>
2020-11-28 20:52:18 -0300

    Strength class created

---
+++ src/Tools/Tool.java
@@ -0,0 +1,14 @@
+package Tools;
+
+import Attributes.Durability;
+import Attributes.Strength;
+import Materials.Material;
+
+public abstract class Tool {
+
+    protected
+
+        Material material;
+        Durability durability;
+        Strength strength;
+}
